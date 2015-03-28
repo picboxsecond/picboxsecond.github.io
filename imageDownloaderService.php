a5d4b1438b833d9732d15d9de52b4647b6703780
picboxfirst <123picbox.first@gmail.com>
2015-03-28 01:48:42 -0400

    autoupdate 2015-03-28-01:48:42

--- imageDownloaderService.php
+++ imageDownloaderService.php
@@ -11,39 +11,40 @@
 	
 	require_once 'imageResizer.php';
 
-	$url = "http://cdn3.123picbox.com/";
+	$url = "http://cdn4.123picbox.com/";
 	// $url = "http://badrullami.github.io/";  
 
 	$query = mysql_query("select * from images where is_downloaded=0 limit 1");
 	
 	while($row = mysql_fetch_array($query)){
 		
+		var_dump($row['id']);
 		mysql_query("update images set is_downloaded=1 where id=".$row['id']);
 
 		$title = beautify($row['title']);
 
 		// original images
 		$file_original = $row['image_id'].'-'.$title."-o.".pathinfo($row['Original'], PATHINFO_EXTENSION);
-		$path_original = "assets/img/original/03/".$file_original;
+		$path_original = "assets/img/original/04/".$file_original;
 		
 		$path_large = "";
 		$path_large2048 = "";
 
 		// medium800
 		$file_medium800 = $row['image_id'].'-'.$title."-vm.".pathinfo($row['Original'], PATHINFO_EXTENSION);
-		$path_medium800 = "assets/img/medium800/03/".$file_medium800;
+		$path_medium800 = "assets/img/medium800/04/".$file_medium800;
 
 		// medium
 		$file_medium = $row['image_id'].'-'.$title."-m.".pathinfo($row['Original'], PATHINFO_EXTENSION);
-		$path_medium = "assets/img/medium/03/".$file_medium;
+		$path_medium = "assets/img/medium/04/".$file_medium;
 
 		// small
 		$file_small = $row['image_id'].'-'.$title."-s.".pathinfo($row['Original'], PATHINFO_EXTENSION);
-		$path_small = "assets/img/small/03/".$file_small;
+		$path_small = "assets/img/small/04/".$file_small;
 
 		// thumbnail
 		$file_thumbnail = $row['image_id'].'-'.$title."-t.".pathinfo($row['Original'], PATHINFO_EXTENSION);
-		$path_thumbnail = "assets/img/thumbnail/03/".$file_thumbnail;
+		$path_thumbnail = "assets/img/thumbnail/04/".$file_thumbnail;
 
 		try {
 			
@@ -75,7 +76,7 @@
 
 					// large
 					$file_large = $row['image_id'].'-'.$title."-l.".pathinfo($row['Original'], PATHINFO_EXTENSION);
-					$path_large = "assets/img/large/03/".$file_large;
+					$path_large = "assets/img/large/04/".$file_large;
 
 					/* resize large 1024 */
 					$width_large = 1024;
@@ -86,7 +87,7 @@
 				if($row['Original_Width'] > 2048) : 
 					// large2048
 					$file_large2048 = $row['image_id'].'-'.$title."-vl.".pathinfo($row['Original'], PATHINFO_EXTENSION);
-					$path_large2048 = "assets/img/large2048/03/".$file_large2048;
+					$path_large2048 = "assets/img/large2048/04/".$file_large2048;
 
 					/* resize large 2048 */
 					$width_large2048 = 2048;
@@ -147,7 +148,7 @@
 				if($row['Original_Height'] > 1024) : 
 					// large
 					$file_large = $row['image_id'].'-'.$title."-l.".pathinfo($row['Original'], PATHINFO_EXTENSION);
-					$path_large = "assets/img/large/03/".$file_large;
+					$path_large = "assets/img/large/04/".$file_large;
 
 					/* resize large 1024 */
 					$height_large = 1024;
@@ -158,7 +159,7 @@
 				if($row['Original_Height'] > 2048) : 
 					// large2048
 					$file_large2048 = $row['image_id'].'-'.$title."-vl.".pathinfo($row['Original'], PATHINFO_EXTENSION);
-					$path_large2048 = "assets/img/large2048/03/".$file_large2048;
+					$path_large2048 = "assets/img/large2048/04/".$file_large2048;
 
 					/* resize large 2048 */
 					$height_large2048 = 2048;
